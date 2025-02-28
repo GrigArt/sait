b5433169a4733d29aebc005315e4b4dd185b5677
GrigArt <37264868+GrigArt@users.noreply.github.com>
2025-02-28 17:08:50 +0300

    Create getPersons.php

---
+++ php/getPersons.php
@@ -0,0 +1,12 @@
+<?php
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+$link = mysqli_connect(...);
+if ($link == false){
+    print("Ошибка: Невозможно подключиться к MySQL " . mysqli_connect_error());
+} else {
+    $sql = "SELECT * FROM Persons";
+    $result = mysqli_query($link, $sql);
+    $resArray = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    //print(json_encode($resArray, JSON_UNESCAPED_UNICODE));   
+}
+?>
